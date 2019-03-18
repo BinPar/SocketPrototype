f9de7ab0d303b5a088057622864e47b1d5696fc3
drnachio <nacho@binpar.com>
2019-03-18 14:54:29 +0100

    sry

--- www/pages/_document.js
+++ www/pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="es">
         <GlobalStyle />
-        <Head>          
+        <Head>
           <meta name="viewport" content="width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
           {this.props.styleTags}
